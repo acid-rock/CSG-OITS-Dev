ae6017913422b131f903adec15af8a17fbd3168f
acid-rock <lorenztuboro00@gmail.com>
2026-02-28 14:43:02 +0800

    test commit for build

--- frontend/src/admin/ProtectedRoute.tsx
+++ frontend/src/admin/ProtectedRoute.tsx
@@ -1,17 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-import App from './AdminPage';
-import '../index.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./AdminPage";
+import "../index.css";
 
 const ProtectedRoute = () => {
-  const [authenticated, setAuthenticated] = useState(true);
-  // change this later a global state handler function to monitor the session of the user
-
   const queryClient = new QueryClient();
-
-  if (!authenticated) {
-    return <div>loading...</div>;
-  }
 
   return (
     <QueryClientProvider client={queryClient}>
